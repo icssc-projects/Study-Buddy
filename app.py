912ae6b9a3faccb7e910c800cf99aeddf89a2057
William Castelli <billycastelli@gmail.com>
2019-01-07 16:25:18 -0800

    Added new post and new user pages

--- app.py
+++ app.py
@@ -24,4 +24,38 @@ def users():
         return str(e)
 
 
-# sudo mysql -u roots
+
+@app.route('/create-account')
+def createAccount():
+    try:
+        ## Use the cursor to insert into User
+        ## Set session variable to logged_in = True, username = ****, uid - ###
+        ## Use these values throughout the session
+        ## Session variables can be accessed in HTML files without passing them
+        return render_template("create-user.html")
+    except Exception as e:
+        return str(e)
+
+@app.route('/posts')
+def posts():
+    try:
+        cursor, connection = connect()
+        cursor.execute("""select * from Post""")
+        posts = cursor.fetchall()
+        return render_template("posts.html", posts = str(posts))
+    except Exception as e:
+        return str(e)
+
+@app.route('/create-post')
+def createPost():
+    try:
+        ## Use the cursor to insert into Post
+        ## Make sure to insert the current session's username and uid
+        return render_template("create-post.html")
+    except Exception as e:
+        return str(e)
+
+
+# The information is accessible now. Post and User are the main objects.
+
+
--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def connect():
-    conn = mysql.connector.connect(user = 'fVb9YnCgUU', password = '########',
+    conn = mysql.connector.connect(user = 'fVb9YnCgUU', password = 'L7AZ3KCccR',
                                    host = 'remotemysql.com', database = 'fVb9YnCgUU',
                                    port = 3306)
     cursor = conn.cursor()
